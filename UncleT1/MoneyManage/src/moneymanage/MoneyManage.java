71a536e97183d7ef5a8e1c7f4ca5ae4cab678808
overbejt <overbejt@miamioh.edu>
2017-12-17 05:41:57 -0500

    Not overcomplicating it

--- UncleT1/MoneyManage/src/moneymanage/MoneyManage.java
+++ UncleT1/MoneyManage/src/moneymanage/MoneyManage.java
@@ -23,6 +23,8 @@ public class MoneyManage extends Application {
         
         Scene scene = new Scene(root);
         
+        
+        
         stage.setScene(scene);
         stage.show();
     }
